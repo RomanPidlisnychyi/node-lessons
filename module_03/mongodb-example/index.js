4ed660ccf678b056d93730dbec3e5822505376a8
RomanPidlisnychyi <58337316+RomanPidlisnychyi@users.noreply.github.com>
2021-01-21 19:20:31 +0200

    Update index.js

--- module_03/mongodb-example/index.js
+++ module_03/mongodb-example/index.js
@@ -5,12 +5,10 @@
 
 const mongodb = require('mongodb');
 const { MongoClient } = mongodb;
-const MONGODB_URL =
-  'mongodb+srv://test_admin:LZbPNjgT9EEhIypX@lucky-mongodb-exapmle.komyo.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const DB_NAME = 'test_db';
 
 async function main() {
-  const client = await MongoClient.connect(MONGODB_URL);
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
   console.log('Successfully conect to DB');
 
   const db = client.db(DB_NAME);
